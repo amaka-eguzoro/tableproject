e872d6cceeedc6f5f04cf7f52bb8a0a297944ab2
Amaka Eguzoro <eguzoroa@gmail.com>
2021-05-31 14:09:24 +0100

    link fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'week2';
+
+
+  userData:any=[
+    {name: "Sandra",addr:"Lagos",profile:"Developer"},
+    {name: "Akash",addr:"Tokyo",profile:"Developer"},
+    {name: "Rajan",addr:"Mumbasa",profile:"Developer"},
+    {name: "Shashank",addr:"Ehiopia",profile:"Developer"},
+    {name: "Sajan",addr:"Kigali",profile:"Developer"}
+  ];
+
+  constructor(){
+    console.log(this.userData);
+  }
 }
+;
